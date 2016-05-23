0559addf6f2da219ba9ef7f8da2cee70e33b755f
Prashant Sahni <prashant.sahni5@gmail.com>
2016-05-23 14:11:57 +0530

    basic css

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,6 @@ module TeamStatus
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    Rails.application.config.assets.precompile += %w( report.css )
   end
 end
