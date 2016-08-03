483a611fa6df7e063a1d82e277a2677c05bfbf48
Prashant Sahni <prashant.sahni5@gmail.com>
2016-08-03 07:32:51 +0100

    root to status new

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  root :to => 'status#index'
+  root :to => 'status#new'
   resources :status do
     collection do
       get :all_status
       get :status_report
+      get :notify_status
     end
   end
 
