5f8ccb176c84e2ea2bb559ba433f6b0b4dc01417
Prashant Sahni <prashant.sahni5@gmail.com>
2016-08-03 07:31:59 +0100

    -> code to add host when starting rails server

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,12 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+#require 'rails/commands/server'
+# module Rails
+#   class Server
+#     def default_options
+#       super.merge(Host:  '172.16.69.164', Port: 3000)
+#     end
+#   end
+# end
